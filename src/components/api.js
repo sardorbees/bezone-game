79d092243c1a2efdba9adf92c38ca7bcdadba073
Sardor Bees <sardor@example.com>
2025-09-05 12:09:46 +0500

    Initial commit

--- src/components/api.js
+++ src/components/api.js
@@ -33,27 +33,27 @@
 
 
 
-import axios from "axios";
+// import axios from "axios";
 
-const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/",
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
+// const API = axios.create({
+//   baseURL: "http://127.0.0.1:8000/",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
 
-API.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("access");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+// API.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem("access");
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => Promise.reject(error)
+// );
 
-export default API;
+// export default API;
 
 // import axios from 'axios';
 
@@ -71,3 +71,30 @@ export default API;
 // });
 
 // export default API;
+
+
+import axios from "axios";
+
+const API = axios.create({
+    baseURL: "http://127.0.0.1:8000/",
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
+});
+
+API.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("access");
+        if (token) {
+            config.headers = {
+                ...config.headers,
+                Authorization: `Bearer ${token}`,
+            };
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
+
+export default API;
